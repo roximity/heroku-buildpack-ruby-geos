03a77bc34a352deaef1c1a48e1be6ebdf868e7ab
Terence Lee <hone02@gmail.com>
2011-12-08 23:58:04 -0800

    debug

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -237,6 +237,7 @@ ERROR
   def clear_gem_specs_cache
     topic("Clearing gem specs cache")
     run("gem sources -c")
+    puts(run("gem spec -r curb-fu --version 0.6.1"))
   end
 
   # runs bundler to install the dependencies
@@ -300,6 +301,8 @@ ERROR
 
         error error_message
       end
+
+      puts run("cat vendor/bundle/ruby/1.9.1/specifications/curb-fu-0.6.1.gemspec")
     end
   end
 
