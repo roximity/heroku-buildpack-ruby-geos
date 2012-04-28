d0a4efd202fed72bfb83a40bc0628decab52f3f1
Ryan Smith (ace hacker) <ryandotsmith@32k.io>
2012-04-28 13:21:03 -0700

    add gdal to path

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -62,7 +62,7 @@ private
   # the base PATH environment variable to be used
   # @return [String] the resulting PATH
   def default_path
-    "bin:#{slug_vendor_base}/bin:/usr/local/bin:/usr/bin:/bin"
+    "bin:#{slug_vendor_base}/bin:/usr/local/bin:/usr/bin:/bin:app/gdal/gdal/bin/"
   end
 
   # the relative path to the bundler directory of gems
